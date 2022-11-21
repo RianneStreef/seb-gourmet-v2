566d32fef038a37bdbf9d76d1ffe1963a3965569
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-11-21 13:27:28 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `seb-gourmet-v2`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
