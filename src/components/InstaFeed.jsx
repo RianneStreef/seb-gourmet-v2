c78167f543b1efcf36562433131ac3bd0c30e123
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2025-11-17 20:28:02 +0100

    no more video

--- src/components/InstaFeed.jsx
+++ src/components/InstaFeed.jsx
@@ -23,7 +23,7 @@ const InstaFeed = (props) => {
       </Helmet>
       <div id="insta" className="insta">
         <h2>{language.instaFeed}</h2>
-        <div className="video-container">
+        {/* <div className="video-container">
           <iframe
             className="you-tube"
             src="https://www.youtube.com/embed/l7S7Rd8ujDQ"
@@ -31,7 +31,7 @@ const InstaFeed = (props) => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
-        </div>
+        </div> */}
         {/* <div data-mc-src="f10e8e66-aada-4742-9b38-f3d4068eec81#instagram"></div> */}
 
         <div
